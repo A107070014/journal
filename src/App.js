45ea3b9e571a5b6f18e536feec3c7bba2e0a41cc
A107070014 <51104270+A107070014@users.noreply.github.com>
2022-04-20 00:09:10 +0800

    20220420

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import trashCan from './img/trash-can.png';
 import close from './img/close.png';
 import calendar from './img/calendar.png';
 import pen from './img/pen.png';
+import back from './img/back.png';
 import autoplay1 from './img/autoplay1.jpg';
 import autoplay2 from './img/autoplay2.jpg';
 import autoplay3 from './img/autoplay3.jpg';
@@ -31,6 +32,9 @@ export default function Journal() {
     speed:5000,
     autoplaySpeed:15000
   };
+
+
+
   return (
     <div className="book" style={{transform:openBook && "translateX(50%)"}}>
       <div className={["cover",openBook && "flipped"].join(' ')} style={{zIndex:openBook && -1}}>
@@ -39,8 +43,13 @@ export default function Journal() {
             <p>我的個人日記</p>   
           </div>
         </div>
-        <div className="back" >
+        <div className="back">
           <div className="back-content">
+            <div className="backArrow" onClick={()=>setOpenBook(!openBook)}>
+              <div className="closeBook" >
+                <img src={back} alt="闔上日記"/>
+              </div>
+            </div>
             <div className="bar">
               <div className="picture">
                 <img src={picture} alt="大頭貼"/>
@@ -56,7 +65,7 @@ export default function Journal() {
           </div>
         </div>
       </div>
-      <div className="closeBtn"><img src={close} alt="取消" width={15}/></div>
+      {openBook &&<div className="closeBtn"><img src={close} alt="取消" width={15}/></div>}
       <div className="page">
         <div className="info">
           <div className="title">
@@ -84,7 +93,7 @@ export default function Journal() {
                 <img src={autoplay4} alt="輪播圖片" />
               </div>
             </Slider>
-          </div>
+          </div>          
           <span className="uploadImage">
             <label for="file-input">
               <img src={image} alt="上傳照片"/>
@@ -93,9 +102,9 @@ export default function Journal() {
           </span>
           <textarea name="journal" value={value} onChange={(e) => setValue(e.target.value)} placeholder='日記...' ></textarea>
         </div>
-        <div className="saveBtn">
+        {openBook && <div className="saveBtn">
           <img src={save} alt="儲存" width={30}/>
-        </div>
+        </div>}
       </div>
       <div className="back-cover"></div>
     </div>
