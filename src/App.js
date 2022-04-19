18c17db7c4ee5984eec5b377045642a4e2b14ed1
A107070014 <51104270+A107070014@users.noreply.github.com>
2022-04-19 17:47:17 +0800

    insert carousel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
 import {useState} from 'react';
 import picture from './img/欣玫.jpg';
-import background from './img/background.png';
 import search from './img/search.png';
+import image from './img/image.png';
 import add from './img/add.png';
 import save from './img/save.png';
 import trashCan from './img/trash-can.png';
 import close from './img/close.png';
 import calendar from './img/calendar.png';
 import pen from './img/pen.png';
+import autoplay1 from './img/autoplay1.jpg';
+import autoplay2 from './img/autoplay2.jpg';
+import autoplay3 from './img/autoplay3.jpg';
+import autoplay4 from './img/autoplay4.jpg';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 
 export default function Journal() {
   const [openBook,setOpenBook] = useState(false); 
   const [value,setValue] = useState('');
+  /*react-slick autoplay*/
+  const settings = {
+    dots:false,
+    infinite:true,
+    slidesToShow:2,
+    slidesToScroll:1,
+    autoplay:true,
+    speed:5000,
+    autoplaySpeed:15000
+  };
   return (
     <div className="book" style={{transform:openBook && "translateX(50%)"}}>
       <div className={["cover",openBook && "flipped"].join(' ')} style={{zIndex:openBook && -1}}>
@@ -53,8 +70,27 @@ export default function Journal() {
         </div>
         <div className="content">
           <div>
-            <img src={background} alt="輪播圖片"/><br/>
+            <Slider {...settings}>
+              <div>
+                <img src={autoplay1} alt="輪播圖片" />
+              </div>
+              <div>
+                <img src={autoplay2} alt="輪播圖片" />
+              </div>
+              <div>
+                <img src={autoplay3} alt="輪播圖片" />
+              </div>
+              <div>
+                <img src={autoplay4} alt="輪播圖片" />
+              </div>
+            </Slider>
           </div>
+          <span className="uploadImage">
+            <label for="file-input">
+              <img src={image} alt="上傳照片"/>
+            </label>
+            <input id="file-input" type="file" accept="image/png,image/jpeg" multiple="multiple"/>
+          </span>
           <textarea name="journal" value={value} onChange={(e) => setValue(e.target.value)} placeholder='日記...' ></textarea>
         </div>
         <div className="saveBtn">
@@ -63,6 +99,7 @@ export default function Journal() {
       </div>
       <div className="back-cover"></div>
     </div>
+    
   );
 }
 
